6f0d3d5de22418f7a2c343fb753591d48ab42a9e
Yue <vluxyue@gmail.com>
2015-12-28 20:30:26 +0800

    Cal.js

---
+++ Cal.js
@@ -0,0 +1,48 @@
+function cal(url) {
+	$.ajax({
+		dataType: "json",
+		url: url,
+		success: function(data) {
+			var date = new Date();
+			var cal = new CalHeatMap();
+			cal.init({
+				itemSelctor: '.cal-heatmap-container', //Dom to insert the cal
+				domain: 'month',
+				subDomain: 'x_day',
+				start: date.setDate(date.getDate() - 180), // Default half year
+
+				data: data, // date to view
+				dataType: 'json',
+				range: 6,
+				// subDomainTextFormat:"%d",
+
+				subDomainTextFormat: function(date, value) {
+					return value;
+				},
+
+				domainLabelFormat: "%m-%Y",
+
+				legendColors: {
+					min: "#f4decd",
+					max: "#ff1a1a",
+					base: "#ededed",
+					empty: "#ededed",
+				},
+				weekStartOnMonday: true,
+
+				//CSS to adjust
+				cellSize: 15,
+				cellRadius: 1,
+				cellPadding: 5,
+				domainMargin: [0, 2],
+				// colLimit:7,
+				tooltip: true,
+				// domainDynamicDimension: true,
+
+			});
+		},
+	});
+	setTimeout(function() {
+		$(".graph>svg>svg>g>rect:not(.r1,.r2,.r3,.r4,.r5)").css("fill", "#ededed");
+	}, 500)
+}
