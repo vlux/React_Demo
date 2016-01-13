3d463beaa184527ff6959c1449df52b0737b4fb8
Yue <vluxyue@gmail.com>
2016-01-13 20:54:49 +0800

    leave the necessity

--- Django_Web/react/static/js/am.js
+++ Django_Web/react/static/js/am.js
@@ -1,106 +1,80 @@
-var chartData = generateChartData();
-var chart = AmCharts.makeChart("chartdiv", {
-	"type": "serial",
-	"theme": "light",
-	"legend": {
-		"useGraphSettings": true
-	},
-	"dataProvider": chartData,
-	"valueAxes": [{
-		"id": "v1",
-		"axisColor": "#FF6600",
-		"axisThickness": 2,
-		"gridAlpha": 0,
-		"axisAlpha": 1,
-		"position": "left"
-	}, {
-		"id": "v2",
-		"axisColor": "#FCD202",
-		"axisThickness": 2,
-		"gridAlpha": 0,
-		"axisAlpha": 1,
-		"position": "right"
-	}, {
-		"id": "v3",
-		"axisColor": "#B0DE09",
-		"axisThickness": 2,
-		"gridAlpha": 0,
-		"offset": 50,
-		"axisAlpha": 1,
-		"position": "left"
-	}],
-	"graphs": [{
-		"valueAxis": "v1",
-		"lineColor": "#FF6600",
-		"bullet": "round",
-		"bulletBorderThickness": 1,
-		"hideBulletsCount": 30,
-		"title": "red line",
-		"valueField": "visits",
-		"fillAlphas": 0
-	}, {
-		"valueAxis": "v2",
-		"lineColor": "#FCD202",
-		"bullet": "square",
-		"bulletBorderThickness": 1,
-		"hideBulletsCount": 30,
-		"title": "yellow line",
-		"valueField": "hits",
-		"fillAlphas": 0
-	}, {
-		"valueAxis": "v3",
-		"lineColor": "#B0DE09",
-		"bullet": "triangleUp",
-		"bulletBorderThickness": 1,
-		"hideBulletsCount": 30,
-		"title": "green line",
-		"valueField": "views",
-		"fillAlphas": 0
-	}],
-	"chartScrollbar": {},
-	"chartCursor": {
-		"cursorPosition": "mouse"
-	},
-	"categoryField": "date",
-	"categoryAxis": {
-		"parseDates": true,
-		"axisColor": "#DADADA",
-		"minorGridEnabled": true
-	},
-	"export": {
-		"enabled": true,
-		"position": "bottom-right"
-	}
-});
+var chartData = generatechartData();
 
-chart.addListener("dataUpdated", zoomChart);
-zoomChart();
-
-// generate some random data, quite different range
-function generateChartData() {
+function generatechartData() {
 	var chartData = [];
 	var firstDate = new Date();
-	firstDate.setDate(firstDate.getDate() - 100);
+	firstDate.setDate(firstDate.getDate() - 150);
 
-	for (var i = 0; i < 100; i++) {
+	for (var i = 0; i < 150; i++) {
+		// we create date objects here. In your data, you can have date strings
+		// and then set format of your dates using chart.dataDateFormat property,
+		// however when possible, use date objects, as this will speed up chart rendering.
 		var newDate = new Date(firstDate);
 		newDate.setDate(newDate.getDate() + i);
 
-		var visits = Math.round(Math.random() * 40) + 100;
-		var hits = Math.round(Math.random() * 80) + 500;
-		var views = Math.round(Math.random() * 6000);
+		var visits = Math.round(Math.random() * 100 - 50);
 
 		chartData.push({
 			date: newDate,
-			visits: visits,
-			hits: hits,
-			views: views
+			visits: visits
 		});
 	}
 	return chartData;
 }
 
+
+var chart = AmCharts.makeChart("chartdiv", {
+	"theme": "light",
+	"type": "serial",
+	// "marginRight": 80,
+	"autoMarginOffset": 20,
+	// "marginTop": 20,
+	"dataProvider": chartData,
+	"valueAxes": [{
+		"id": "v1",
+		"axisAlpha": 0.1
+	}],
+	"graphs": [{
+		"balloonText": "[[category]]<br><b>value: [[value]]</b>",
+		"lineColor": "#67b7dc",
+		"valueField": "visits",
+		// "bullet": "round",
+		// "bulletBorderAlpha": 0.5,
+		// "bulletBorderColor": "#FFFFFF",
+		//"hideBulletsCount": 50,
+		//"lineThickness": 2,
+		//"useNegativeColorIfDown": true,
+		//"negativeLineColor": "#fdd400",
+	}],
+	"chartScrollbar": {
+		"scrollbarHeight": 5,
+		"backgroundAlpha": 0.1,
+		"backgroundColor": "#868686",
+		"selectedBackgroundColor": "#67b7dc",
+		"selectedBackgroundAlpha": 1
+	},
+	"chartCursor": {
+		"valueLineEnabled": true,
+		"valueLineBalloonEnabled": true
+	},
+	"categoryField": "date",
+	"categoryAxis": {
+		"parseDates": true,
+		"axisAlpha": 0,
+		"minHorizontalGap": 60,
+		"axisColor": "#DADADA",
+		"minorGridEnabled": true
+	},
+	"export": {
+		"enabled": true
+	}
+});
+
+chart.addListener("dataUpdated", zoomChart);
+//zoomChart();
+
 function zoomChart() {
-	chart.zoomToIndexes(chart.dataProvider.length - 20, chart.dataProvider.length -
-		1);
+	if (chart.zoomToIndexes) {
+		chart.zoomToIndexes(130, chartData.length - 1);
+	}
 }
